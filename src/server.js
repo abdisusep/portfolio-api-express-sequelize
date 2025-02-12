ae47cb83c4f026235dd4419917762af6fa5593c4
Susep Sp <susepsupriatna@gmail.com>
2025-02-13 00:47:27 +0700

    Update module to commonjs

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
-import express from "express";
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello world!"
-    });
-});
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
